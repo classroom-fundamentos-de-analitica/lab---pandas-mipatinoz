b7b8f3749e44d33ecaaa6cac17f1078cbd8a4244
Miguel Patiño <112825575+mipatinoz@users.noreply.github.com>
2022-11-13 01:29:54 -0500

    pandas v2

--- preguntas.py
+++ preguntas.py
@@ -132,15 +132,15 @@ def pregunta_10():
     2   C                    0:5:6:7:9
      3   D                  1:2:3:5:5:7
      4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
-     """
+    """
 
 
-tablaN = tbl0[["_c1", "_c2"]].copy().set_index("_c2").groupby("_c1")
-     proc = {g:":".join(sorted([str(x) for x in c])) for g,c in tablaN.groups.items()}
-     return pd.DataFrame({"_c1":proc.keys(), "_c2":proc.values()}).set_index("_c1")
+    tablaN = tbl0[["_c1", "_c2"]].copy().set_index("_c2").groupby("_c1")
+    proc = {g:":".join(sorted([str(x) for x in c])) for g,c in tablaN.groups.items()}
+    return pd.DataFrame({"_c1":proc.keys(), "_c2":proc.values()}).set_index("_c1")
 
  def pregunta_11():
-     """
+    """
     Construya una tabla que contenga _c0 y una lista separada por ',' de los valores de
     la columna _c4 del archivo `tbl1.tsv`.
     Rta/
